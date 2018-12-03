35efb22460e28e4d34004416e3e08c85a470f3a3
bobby nur febriyanto <bobby.febriyanto@students.amikom.ac.id>
2018-12-03 16:48:39 +0700

    tambah output di Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ public class Hello{
 	public static void main (String[] args) {
 		// TODO Auto-genersted method stub
 		System.out.println("Hello Man!");
+		
+		System.out.println("Udah punya pacar belum Mbk asdos?")
 
 	}
 }
