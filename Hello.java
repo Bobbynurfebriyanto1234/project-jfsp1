06a7d6ddda0ede570ca3a0be1abc2619624175cf
bobbynurfebriyanto <bobby.febriyanto@students.amikom.ac.id>
2018-12-10 16:16:24 +0700

    Sukses

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 public class Hello{
 
 	public static void main (String[] args) {
-		// TODO Auto-genersted method stub
 		System.out.println("Hello Man!");
 		
 		System.out.println("Udah punya pacar belum Mbk asdos?")
